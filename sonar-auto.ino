a618bd7627772778d661e79e73dd3d5154ef3e1c
boothinator <boothinator@users.noreply.github.com>
2021-02-04 18:56:17 -0500

    Drive until 10cm away from obstacle

---
+++ sonar-auto.ino
@@ -0,0 +1,123 @@
+#include <Servo.h>
+
+const int SERVO_PIN = 3;
+const int MIN_ANGLE_PULSE = 700;
+const int MAX_ANGLE_PULSE = 2400;
+const int SONAR_ECHO_PIN = A4;  
+const int SONAR_TRIGGER_PIN = A5; 
+
+Servo servo;
+
+// Measure distance in centimeters
+int measureDistance() {
+  digitalWrite(SONAR_TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(SONAR_TRIGGER_PIN, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(SONAR_TRIGGER_PIN, LOW);
+
+  unsigned long distancePulse = pulseIn(SONAR_ECHO_PIN, HIGH);
+  
+  int distanceCentimeters = (int)(distancePulse / 58);
+  return distanceCentimeters;
+}
+
+void driveForward() {
+  /* Left motors */
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  analogWrite(5, 128);
+  
+  /* Right motors */
+  digitalWrite(9, LOW);
+  digitalWrite(11, HIGH);
+  analogWrite(6, 128);
+}
+
+void driveBackward() {
+  /* Left motors */
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  analogWrite(5, 128);
+  
+  /* Right motors */
+  digitalWrite(9, HIGH);
+  digitalWrite(11, LOW);
+  analogWrite(6, 128);
+}
+
+void coast() {
+  /* Left motors */
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  
+  /* Right motors */
+  digitalWrite(9, LOW);
+  digitalWrite(11, LOW);
+}
+
+void brake() {
+  /* Left motors */
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  
+  /* Right motors */
+  digitalWrite(9, HIGH);
+  digitalWrite(11, HIGH);
+}
+
+void turnLeft() {
+  /* Left motors */
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  analogWrite(5, 128);
+  
+  /* Right motors */
+  digitalWrite(9, LOW);
+  digitalWrite(11, HIGH);
+  analogWrite(6, 128);
+}
+
+void turnRight() {
+  /* Left motors */
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  analogWrite(5, 128);
+  
+  /* Right motors */
+  digitalWrite(9, HIGH);
+  digitalWrite(11, LOW);
+  analogWrite(6, 128);
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+  //servo.attach(SERVO_PIN, MIN_ANGLE_PULSE, MAX_ANGLE_PULSE);
+
+  // Set up drive motor pins
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(6, OUTPUT);
+
+  pinMode(SONAR_TRIGGER_PIN, OUTPUT);
+  pinMode(SONAR_ECHO_PIN, INPUT);
+}
+
+void loop() {
+
+  // Drive forward until distance is less than 10cm
+  int distanceCentimeters = measureDistance();
+  Serial.println(distanceCentimeters);
+
+  if (distanceCentimeters < 10) {
+    brake();
+  } else {
+    driveForward();
+  }
+
+  delay(50);
+}
